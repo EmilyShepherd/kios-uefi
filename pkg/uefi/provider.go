b99b074c0a8920c7f5476324a889efb996612295
Emily Shepherd <emily@redcoat.dev>
2023-11-10 15:23:45 +0000

    Refactor UEFI code into seperate file

--- pkg/uefi/provider.go
+++ pkg/uefi/provider.go
@@ -1,55 +1,28 @@
 package uefi
 
 import (
-	"os"
 	"strings"
-	"syscall"
 
 	"github.com/EmilyShepherd/kios-go-sdk/pkg/bootstrap"
 	kubeconfig "k8s.io/client-go/tools/clientcmd/api/v1"
-	"k8s.io/klog/v2"
 	kubelet "k8s.io/kubelet/config/v1beta1"
 )
-
-const KiosGuid = "3a3ae00f-cf4d-4977-8766-8ea575f6bf4f"
-const CACert = "Kube-CA-Cert"
-const CAKey = "Kube-CA-Key"
-const Hostname = "Hostname"
-const ApiEndpoint = "Kube-Api-Endpoint"
-
-const EfiVarsDirectory = "/sys/firmware/efi/efivars"
-const BootCurrent = "BootCurrent-8be4df61-93ca-11d2-aa0d-00e098032b8c"
-const EfiVarFs = "efivarfs"
 
 type Provider struct {
 }
 
 func (p *Provider) Init() error {
-	if _, err := os.Stat(EfiVarsDirectory + "/" + BootCurrent); err == nil {
-		klog.Info("Efivars is already mounted")
-		return nil
-	}
-
-	syscall.Mount(EfiVarFs, EfiVarsDirectory, EfiVarFs, 0, "")
-
-	return nil
+	return EnsureEfiVarFs()
 }
 
-func readVar(name string) []byte {
-	data, err := os.ReadFile(EfiVarsDirectory + "/" + name + "-" + KiosGuid)
-	if err != nil {
-		return []byte{}
-	}
-
-	// Skip the first 4 bytes as these are UEFI Variable Attributes
-	return data[4:]
-}
+const CACert = "Kube-CA-Cert"
+const CAKey = "Kube-CA-Key"
 
 func (p *Provider) GetClusterCA() bootstrap.Cert {
 	ca := bootstrap.Cert{}
 
-	ca.Cert = readVar(CACert)
-	ca.Key = readVar(CAKey)
+	ca.Cert = GetValue(CACert)
+	ca.Key = GetValue(CAKey)
 
 	return ca
 }
@@ -64,37 +37,29 @@ func (p *Provider) GetCredentialProviders() []kubelet.CredentialProvider {
 	return []kubelet.CredentialProvider{kubelet.CredentialProvider{}}
 }
 
+const Hostname = "Hostname"
+
 func (p *Provider) GetHostname() string {
-	return string(readVar(Hostname))
+	return string(GetValue(Hostname))
 }
 
-func (p *Provider) GetNodeLabels() map[string]string {
-	labels := make(map[string]string)
 
-	prefix := "Node-Label-"
-	guidLength := len(KiosGuid) + 1
-	prefixLength := len(prefix)
+func (p *Provider) GetNodeLabels() (labels map[string]string) {
+	l := GetMultiValue("Node-Label-")
+	labels = make(map[string]string, len(l))
 
-	entries, _ := os.ReadDir(EfiVarsDirectory)
-	for _, file := range entries {
-		name := file.Name()
-		contentLength := len(name) - guidLength
-
-		if len(name) <= guidLength+prefixLength {
-			continue
-		}
-		if name[:prefixLength] == prefix && name[contentLength:] == "-"+KiosGuid {
-			name = name[:contentLength]
-			key := strings.ReplaceAll(name[prefixLength:], "_", "/")
-			labels[key] = string(readVar(name))
-		}
+	for key, value := range l {
+		key = strings.ReplaceAll(key, "_", "/")
+		labels[key] = string(value)
 	}
 
-	return labels
+	return
 }
 
+const ApiEndpoint = "Kube-Api-Endpoint"
+
 func (p *Provider) GetClusterEndpoint() string {
-	return string(readVar(ApiEndpoint))
+	return string(GetValue(ApiEndpoint))
 }
 
 func (p *Provider) GetContainerRuntimeConfiguration() bootstrap.ContainerRuntimeConfiguration {
---
+++ pkg/uefi/uefi.go
@@ -0,0 +1,59 @@
+package uefi
+
+import (
+	"os"
+	"syscall"
+
+	"k8s.io/klog/v2"
+)
+
+const KiosGuid = "3a3ae00f-cf4d-4977-8766-8ea575f6bf4f"
+
+const EfiVarsDirectory = "/sys/firmware/efi/efivars"
+const BootCurrent = "BootCurrent-8be4df61-93ca-11d2-aa0d-00e098032b8c"
+const EfiVarFs = "efivarfs"
+
+func EnsureEfiVarFs() error {
+	if _, err := os.Stat(EfiVarsDirectory + "/" + BootCurrent); err == nil {
+		klog.Info("Efivars is already mounted")
+		return nil
+	}
+
+	syscall.Mount(EfiVarFs, EfiVarsDirectory, EfiVarFs, 0, "")
+
+	return nil
+}
+
+func GetValue(name string) []byte {
+	data, err := os.ReadFile(EfiVarsDirectory + "/" + name + "-" + KiosGuid)
+	if err != nil {
+		return []byte{}
+	}
+
+	// Skip the first 4 bytes as these are UEFI Variable Attributes
+	return data[4:]
+}
+
+func GetMultiValue(prefix string) map[string][]byte {
+	values := make(map[string][]byte)
+
+	guidLength := len(KiosGuid) + 1
+	prefixLength := len(prefix)
+
+	entries, _ := os.ReadDir(EfiVarsDirectory)
+	for _, file := range entries {
+		name := file.Name()
+		contentLength := len(name) - guidLength
+
+		if len(name) <= guidLength+prefixLength {
+			continue
+		}
+		if name[:prefixLength] == prefix && name[contentLength:] == "-"+KiosGuid {
+			name = name[:contentLength]
+			key := name[prefixLength:]
+			values[key] = GetValue(name)
+		}
+	}
+
+	return values
+}
